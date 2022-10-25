92b169f05084917090d4edfdddaab3aaa3560766
Elif Ozbey <eozbey2002@hotmail.com>
2022-10-25 00:22:16 -0400

    Autofac/Validation

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,14 +1,20 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
@@ -21,13 +27,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
+            //business codes
+            //validation -- dogrulama kodu
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Masseges.ProductNameInvalid);
-            }
-            //business kodlar buraya yazilir
+
             _productDal.Add(product);
             return new SuccessResult(Masseges.ProductAdded); //bunu boyle yapabilmenin yolu konstraktir eklemektir. Konstraktir eklemeden Result a parametre gonderemeyiz.
         }
---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,38 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new System.Exception("Bu bir dogrulama sinifi degildir");
+            }
+
+            _validatorType = validatorType;
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType.GetGenericArguments()[0]; //validatortype in base taypini bul (abstract validatir),
+            //onun generic argumanlarindan ilkini bul
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);//ilgili merhodun parametrelerini bul (invocation method demek
+              //Validaterin tipine esit olan parametreleri git bul                                                                       )
+            foreach (var entity in entities)//her birini tek tek gez
+            {
+                ValidationTool.Validate(validator, entity);//validation toolu kullanarak validate et
+            }
+        }
+    }
+}
